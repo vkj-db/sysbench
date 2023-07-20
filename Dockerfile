0dece9fe9f68f9c7898612001170188cdbeefe3f
VikasJain <vikas.jain@databricks.com>
2023-07-19 18:18:06 -0700

    optimize prepare tables

--- Dockerfile
+++ Dockerfile
@@ -10,19 +10,19 @@ RUN apt-get -y install libmysqlclient-dev libssl-dev
 # For PostgreSQL support
 RUN apt-get -y install libpq-dev
 
+RUN apt-get -y install nano mysql-client
+
 ADD https://api.github.com/repos/vkj-db/sysbench/git/refs/heads/modifications version.json
 RUN git clone -b modifications https://github.com/vkj-db/sysbench.git sysbench
 
 WORKDIR sysbench
 RUN ./autogen.sh
-RUN ./configure --with-mysql --with-pgsql
+RUN ./configure --with-mysql
 RUN make -j
 RUN make install
 
 WORKDIR /root
 RUN rm -rf sysbench
-
-RUN apt-get -y install nano mysql-client
 
 COPY run.sh .
 
--- run.sh
+++ run.sh
@@ -12,48 +12,46 @@ set -e -x
 #
 # NOTE: This script assumes that the db is already created and populated with data.
 
+if [[ -z "${db_address}" ]]; then
+  echo "db_address is not set"
+  exit 1
+fi
+
+if [[ -z "${db_password}" ]]; then
+  echo "db_password is not set"
+  exit 1
+fi
+
 db_user="root"
 db_port=3306
 db_driver="mysql"
+table_size=${TABLE_SIZE:-10000000}
+tables=${TABLES:-50}
+time=${TIME:-60}
+report_interval=${REPORT_INTERVAL:-10}
 
-if [[ -z "${TABLE_SIZE}" ]]; then
-  table_size=10000000
-else
-  table_size="${TABLE_SIZE}"
-fi
-
-if [[ -z "${TABLES}" ]]; then
-  tables=50
-else
-  tables="${TABLES}"
-fi
-
-if [[ -z "${TIME}" ]]; then
-  time=60
-else
-  time="${TIME}"
-fi
-
-if [[ -z "${REPORT_INTERVAL}" ]]; then
-  report_interval=10
-else
-  report_interval="${REPORT_INTERVAL}"
-fi
-
-# mode: prepare or run
+# mode: clean, prepare, run
 mode="${MODE:-run}"
 
-# if condition for mode == prepare
+# Clean DB
+if [[ "${mode}" == "clean" ]]; then
+  mysql -h "${db_address}" -P $db_port -u $db_user --password="${db_password}" \
+    -e "drop database IF EXISTS sysbench; create database sysbench";
+  exit 0
+fi
+
+# Prepare DB
 if [[ "${mode}" == "prepare" ]]; then
   # Run prepare
-  sysbench --"${db_driver}"-host="${db_address}" --"${db_driver}"-port="${db_port}" --"${db_driver}"-user="${db_user}" --"${db_driver}"-password="${db_password}" --"${db_driver}"-db=sysbench --db-driver="${db_driver}" --table-size="${table_size}" --tables="${tables}" --time="${time}" --report-interval="${report_interval}" --threads="${tables}" \
+  time sysbench --"${db_driver}"-host="${db_address}" --"${db_driver}"-port="${db_port}" --"${db_driver}"-user="${db_user}" --"${db_driver}"-password="${db_password}" --"${db_driver}"-db=sysbench --db-driver="${db_driver}" --table-size="${table_size}" --tables="${tables}" --time="${time}" --report-interval="${report_interval}" --threads="${tables}" \
   --create-secondary=false \
   oltp_point_select prepare
   exit 0
 fi
 
+# Run benchmark
 run_bench() {
-  sysbench --"${db_driver}"-host="${db_address}" --"${db_driver}"-port="${db_port}" --"${db_driver}"-user="${db_user}" --"${db_driver}"-password="${db_password}" --"${db_driver}"-db=sysbench --db-driver="${db_driver}" --table-size="${table_size}" --tables="${tables}" --time="${time}" --report-interval="${report_interval}" \
+  time sysbench --"${db_driver}"-host="${db_address}" --"${db_driver}"-port="${db_port}" --"${db_driver}"-user="${db_user}" --"${db_driver}"-password="${db_password}" --"${db_driver}"-db=sysbench --db-driver="${db_driver}" --table-size="${table_size}" --tables="${tables}" --time="${time}" --report-interval="${report_interval}" \
   --threads="$2" "$1" run
 }
 
